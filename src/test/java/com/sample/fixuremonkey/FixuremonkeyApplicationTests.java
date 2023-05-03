217dd60ad7e13f30f1c67435cc75af103e02fa2a
Hanjione <gkswldnj0803@gmail.com>
2023-05-03 12:54:43 +0900

    Initial commit

---
+++ src/test/java/com/sample/fixuremonkey/FixuremonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.fixuremonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixuremonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
